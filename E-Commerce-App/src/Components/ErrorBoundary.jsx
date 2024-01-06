7d218726067c68e44cc26cac1a8896ddb4051564
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-01-06 19:29:04 +0530

    added ErrorBoundary

---
+++ E-Commerce-App/src/Components/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by error boundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong. Please try again later.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
