4a6ea30a37647d7a259217bed9f6d8c1ead5430b
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-01-06 19:31:50 +0530

    routes updated

--- E-Commerce-App/src/App.jsx
+++ E-Commerce-App/src/App.jsx
@@ -1,11 +1,37 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Navigation from "./Components/Navigation";
+import Cart from "./Components/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const addToCart = (id) => {
+    console.log(id);
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navigation></Navigation>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home addToCart={addToCart} data={data} />} />
+        <Route path="/cart" element={<Cart data={data} />} />
+      </Routes>
     </>
   );
 }
